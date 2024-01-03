0764c96530287800af35f22c63c97bd2239c1997
Hawii02 <leon.onditi@student.moringaschool.com>
2024-01-03 18:30:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(response => {
+    return response.json();
+    })
+    .then(data => {
+      const booksArray = Array.isArray(data) ? data : data.books || [];
+      renderBooks(booksArray);
+    });
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = book.name || book.title || "Untitled";
     main.appendChild(h2);
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+   try {
+    fetchBooks();
+  } catch (error) {
+    console.error('There was an error:', error);
+  }
 });
